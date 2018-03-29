7739507b9ac6d3b8a7248f4c66117c70ade3550f
fond <gregorysallen.e@gmail.com>
2018-03-29 15:14:38 +0200

    modif structure

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 datadir = os.path.join(basedir,"rvaapp", "data")
 DATAFLIGHT = os.path.join(datadir, 'data_lieux.csv')
-STRUCT = os.path.join(datadir, 'structureDF.csv')
+STRUCT = os.path.join(datadir, 'structure.csv')
 DH = os.path.join(datadir, 'decalage_horaire.csv')
 SCALER = os.path.join(datadir, 'scaler.pkl')
 MODELE = os.path.join(datadir, 'modele.pkl')
